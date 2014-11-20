93ad4a2c4de667177a92617bb7199edd6d577af4
marjola3 <marjola3@poczta.onet.pl>
2014-11-20 21:48:51 +0100

    Initial commit

---
+++ src/main/java/pl/mpolak/forum/controller/IndexController.java
@@ -0,0 +1,20 @@
+package pl.mpolak.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printWelcome() {
+        return "index";
+    }
+
+}
